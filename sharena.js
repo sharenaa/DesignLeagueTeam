1966d35d9099e5afd8409f3491c56927004ff05e
sharena <144714984+sharenaa@users.noreply.github.com>
2024-04-17 12:06:29 -0400

    Create sharena.js

---
+++ sharena.js
@@ -0,0 +1,10 @@
+//LEader
+window.addEventListener('load', () => {
+  const loader = document.querySelector('.loader');
+  setTimeout(() => {
+        loader.setAttribute('style', 'opacity: 0;');
+  }, 500);
+  setTimeout(() => {
+    loader.parentNode.removeChild(loader);
+    document.body.style.overflow = 'initial';
+  }, 1000);
