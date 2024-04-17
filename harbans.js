9a4cadf95fb7ef4b43c969867db1be3719b2c6d6
HarbansS <144715114+HarbansS@users.noreply.github.com>
2024-04-17 12:09:50 -0400

    Create harbans.js

---
+++ harbans.js
@@ -0,0 +1,11 @@
+//Loader
+window.addEventListener('load', ()=> {
+        const loader = document.querySelector('loader');
+        setTimeout(() => {
+                loader.setAttribute('style', 'opacity: 0;');
+        }. 500);
+        setTimeout(() => {
+                loader.parentNode.removeChild(loader);
+                document.body.style.overflow = 'initial';
+        }, 100);
+});
