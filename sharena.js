ed6359692781f8ac02ec160293c4463d4f23a136
sharena <144714984+sharenaa@users.noreply.github.com>
2024-04-19 12:51:18 -0400

    Update sharena.js

--- sharena.js
+++ sharena.js
@@ -7,4 +7,35 @@ window.addEventListener('load', () => {
   setTimeout(() => {
     loader.parentNode.removeChild(loader);
     document.body.style.overflow = 'initial';
-  }, 1000);
+  }, 100);
+});
+
+const coinSfx = new Sfx('sfx/Mario Coin.wav');
+const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
+const powerUpSfx = new Sfx('sfx/Mario Powerup.wav');
+const container = document.querySelector('.content');
+let ninja;
+
+function Sfx(src) {
+  this.sound = document.createElement('audio');
+  this.sound.src = src;
+  this.sound.setAttribute = ('preload', 'auto');
+  this.sound.setAttribute = ('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }:
+}
+
+function Ninjas(name, age, height, color) {
+  this.name = name;
+  this.age = age;
+  this.height - height;
+  this.color = color;
+
+  this.coin = () => {
+    //audio
+    coinSfx.play();
+    
+  
