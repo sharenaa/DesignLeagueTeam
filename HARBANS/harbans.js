fc049b246682683b8026254c7bd162e4bd03700f
HarbansS <144715114+HarbansS@users.noreply.github.com>
2024-04-19 12:50:57 -0400

    Update harbans.js

--- HARBANS/harbans.js
+++ HARBANS/harbans.js
@@ -9,3 +9,32 @@ window.addEventListener('load', ()=> {
                 document.body.style.overflow = 'initial';
         }, 100);
 });
+
+const coinSfx = new Sfx('sfx/Mario Coin.wav');
+const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
+const powerUpSfx = new Sfx('sfx/Mario Powerup.wav');
+const container = document.querySelector('.content');
+let ninja;
+
+function Sfx(src) {
+        this.sound = document.createElement('audio');
+        this.sound/sound.src = src;
+        this.sound.setAttribute('preload', 'auto');
+        this.sound.setAttribute('controls', 'none');
+        this.sound.style.display = 'none';
+        document.body.appendChild(this.sound);
+        this.play = function() {
+                this.sound.play();
+        }
+}
+
+function Ninjas(name, age, height, color) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.color = color;
+
+        this.coin = () => {
+                
+        }
+}
