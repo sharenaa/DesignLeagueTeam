00b334daa8b6cfa619084b4fb3ffd6f375f7db5c
HarbansS <144715114+HarbansS@users.noreply.github.com>
2024-05-07 18:44:29 -0400

    Update Harbans.js

--- HARBANS/Harbans.js
+++ HARBANS/Harbans.js
@@ -1,4 +1,4 @@
-
+//Game Content
 const coinSfx = new Sfx('sfx/Mario Coin.wav');
 const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
 const powerUpSfx = new Sfx('sfx/Mario Powerup.wav');
@@ -7,14 +7,14 @@ let ninja;
 
 function Sfx(src) {
         this.sound = document.createElement('audio');
-        this.sound/sound.src = src;
+        this.sound.src = src;
         this.sound.setAttribute('preload', 'auto');
         this.sound.setAttribute('controls', 'none');
         this.sound.style.display = 'none';
         document.body.appendChild(this.sound);
         this.play = function() {
                 this.sound.play();
-        }
+        };
 }
 
 function Ninjas(name, age, height, color) {
@@ -23,30 +23,29 @@ function Ninjas(name, age, height, color) {
         this.height = height;
         this.color = color;
 
-        this.coin = () =>{
+        this.coin = () => {
                 //audio
                 coinSfx.play();
+                const button = document.querySelector('#btnCoin');
 
-        const button document.querySelector('#btnCoin');
+                button.style.setProperty('background-color', '#2ecc71');
+                button.style.setProperty('border-color', '#2ecc71');
+                button.style.setProperty('color', 'var(--bg-color)');
 
-        button.style.setProperty('background-color', '#2ecc71');
-        button.style.setProperty('border-color', '2ecc71');
-        button.style.setProperty('color', 'var(--bg-color');
+                //coin effect
+                const coinImg = document.createElement('img');
+                coinImg.setAttribute('src', 'imgs/coin.png');
+                coinImg.setAttribute('id', 'coin-img');
+                coinImg.style.transform = 'translate(-50%,calc(-100% - ' + ninja.height + 'px)';
+                container.appendChild(coinImg);
 
-        //coin effect
-        const coinImg = document.createElement('img');
-        coinImg.setAttribute('src', 'imgs/coins.png');
-        coinImg.setAttribute('id', 'coin-img');
-        coin.Img.style.transform = 'translate(-50%,calc(-100% - ' + ninja.height + 'px)';
-        container.appendChild(coinImg);
-
-        $('coin-img').animate({ top: '60%', opacity: 1 }, 250);
-        $('coin-img').animate({ top: '70%', opacity: 0 }, 250, () => {
-                $('coin-img').remove();
-                button.style.setProperty('background-color', 'transparent');
-                button.style.setProperty('border-color', 'var(--accent-color)');
-                button.style.setProperty('color', 'var(--accent-color');
-        });
+                $('#coin-img').animate({ top: '60%', opacity: 1 }, 250);
+                $('#coin-img').animate({ top: '70%', opacity: 0}, 250, () => {
+                        $('coin-img').remove();
+                        button.style.setProperty('background-color', 'transparent');
+                        button.style.setProperty('border-color', 'var(--accent-color)');
+                        button.style.setProperty('color', 'var(--accent-color)');
+                });
         };
         this.pipe = () => {
                 pipeSfx.play();
@@ -67,7 +66,7 @@ const promptUser = () => {
                 age = parseInt(age);
                 if (isNaN(age)) {
                         this.getAge();
-                } else{
+                } else {
                         return;
                 }
         };
@@ -75,10 +74,10 @@ const promptUser = () => {
         this.getHeight = () => {
                 height = prompt('Pick height: 1, 2, 3, 4') || 4;
                 height = parseInt(height);
-                if (height === 1 || height === 2|| height === 3|| height === 4) {
+                if (height === 1 || height === 2 || height === 3 || height === 4) {
                         height = height * 64;
                         return;
-                } else{
+                } else {
                         this.getHeight();
                 }
         };
@@ -97,42 +96,41 @@ const promptUser = () => {
                 img = document.createElement('img');
                 let size = 'width:' + height + 'px;height:' + height + 'px;';
                 if (color === 'blue') {
-                        img.setAttribute('src', 'imgs/ninja1.png);
+                        img.setAttribute('src', 'img/ninja1.png');
                         img.setAttribute('style', size);
                         container.appendChild(img);
                 } else if (color === 'green') {
-                        img.setAttribute('src', 'img/ninja2.png);
+                        img.setAttribute('src', 'img/ninja2.png');
                         img.setAttribute('style', size);
                         container.appendChild(img);
                 } else if (color === 'red') {
-                        img.setAttribute('src', 'img/ninja3.png);
+                        img.setAttribute('src', 'img/ninja3.png');
                         img.setAttribute('style', size);
                         container.appendChild(img);
                 }
         };
 
         this.changeBtns = () => {
-                const btn = document.queryselector('button');
+                const btn = document.querySelector('button');
                 const btnContainer = document.querySelector('.btns-container');
-                btnContainer.removeChild(btn)
-                btnContainer.innerHTML = 
-                        "<button onclick='ninja.coin()' id='btnCoin'>Coin</button> \n <button onclick='ninjapipe()' id='btnPipe'>Pipe</button> \n <button onclick='ninja.powerUp()' id='btnPowerUp'>Power Up</button>";
+                btnContainer.removeChild(btn);
+                btnContainer.innerHTML =
+                        "<button onclick='ninja.coin()' id='btnCoin'>Coin</button> \n <button onclick='ninja.pipe()' id='btnPipe'>Pipe</button> \n <button onclick='ninja.powerUp()' id='btnPowerUp'>Power Up</button>";
                 const newBtns = document.querySelectorAll('button');
                 for (let i = 0; i < newBtns.length; i++) {
-                        newBtns[i].style.setProperty('margin-left', '1.5rem');
+                        newBtns [I].style.setProperty('margin-left', '1.5rem');
                 }
 
                 // Add floor
                 const floor = document.createElement('div');
                 floor.setAttribute('class', 'floor');
                 container.appendChild(floor);
-                floor.innerHTML = 
+                floor.innerHTML =
                         'Name: ' +
                         ninja.name +
                         '\xa0\xa0\xa0Age: ' +
                         ninja.age +
                         '\xa0\xa0\xa0Height: ' +
-                        ninja.height + 
                         'px' +
                         '\xa0\xa0\xa0Color: ' +
                         ninja.color;
@@ -142,7 +140,7 @@ const promptUser = () => {
         this.getHeight();
         this.getColor();
 
-        ninja = new Ninjas(name, age, height color);
+        ninja = new Ninjas(name, age, height, color);
 
         this.appendImg();
         this.changeBtns();
